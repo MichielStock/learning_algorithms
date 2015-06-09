e63e8be251fd6aa6f37eb445ba8607f46d07b6ea
MichielStock <michiel.stock@telenet.be>
2015-06-09 19:27:32 +0200

    version of enhanced threshold

--- Efficient_querying/EfficientInference.py
+++ Efficient_querying/EfficientInference.py
@@ -10,6 +10,43 @@ seperable linear models
 """
 
 from numpy import dot
+from numba import jit
+
+def calculate_partial_score(r, pos, xi, Y, sorted_lists):
+    """
+    calculates a partial score for the position in the list
+    """
+    indice = sorted_lists[conditionalposition(pos, xi), r]
+    return xi * Y[indice, r]
+
+def swap(l, i, j):
+    # swaps two elements on a postion of a list
+    temp = l[i]
+    l[i] = l[j]
+    l[j] = temp
+
+def conditionalposition(pos, xi):
+    """
+    returns position such that:
+            pos if xi < 0
+            - pos - 1 if  xi >= 0
+    """
+    if xi < 0:
+        return pos
+    else:
+        return - pos - 1
+
+def cure_partial_ordered_list(posset):
+    """
+    Sorts a partial ordered list for which only the first element is out of
+    place
+    """
+    n = len(posset)
+    for i in range(n-1):
+        if posset[i][0] < posset[i+1][0]: # elements are not ordered
+            swap(posset, i, i+1)
+        else:
+            break
 
 class TopKInference():
     """
@@ -57,21 +94,129 @@ class TopKInference():
                 n_items_scored)
             n_items_scored += 1
         if count_calculations:
+            return top_list, n_items_scored
+        else:
+            return top_list
+
+    def get_top_K_fagin(self, x_u, K=1, count_calculations=False):
+        """
+        Returns top-K using Fagin's algorithm
+        """
+        top_list = []
+        n_items_scored = 0
+
+        # fill in
+        if count_calculations:
             return top_list, count_calculations
         else:
             return top_list
+
+    def get_top_K_threshold(self, x_u, K=1, count_calculations=False):
+        """
+        Returns top-K using the threshold algorithm
+        """
+        top_list = []
+        n_items_scored = 0
+        neg_elements_query = set([i for i, el in enumerate(x_u) if el < 0])
+        non_zero_elements_query = [i for i, el in enumerate(x_u) if el != 0]
+        scored = set([])
+        upper_bound = 1
+        lower_bound = 0
+        depth = 0
+        while upper_bound > lower_bound:
+            upper_bound = 0
+            for r in non_zero_elements_query:
+                if r in neg_elements_query:
+                    item = self.sorted_lists[depth, r]  # negative, so start from
+                            # items with the LOWEST score for this item
+                else:
+                    item = self.sorted_lists[-(depth+1), r]
+                    # update upper bound
+                upper_bound += self.Y[item, r] * x_u[r]
+                if item not in scored:
+                    new_scored_item = self.score_item(x_u, item)
+                    if n_items_scored < K or lower_bound < new_scored_item[0]:
+                            self.update_top_list(top_list, new_scored_item, K,
+                                n_items_scored)
+                    n_items_scored += 1
+                    scored.add(item)
+            # update lower bound
+            if n_items_scored >= K:
+                lower_bound = top_list[0][0]
+            depth += 1
+        if count_calculations:
+            return top_list, n_items_scored
+        else:
+            return top_list
+
+    def get_top_K_threshold_enhanced(self, x_u, K=1, count_calculations=False):
+        """
+        Returns top-K using the modified threshold algorithm
+        """
+        top_list = []
+        n_items_scored = 0
+        # initiate list with rules
+        # contains tuples with
+        # (paritial score, xi, r, position_sorted_list, decr/incr sorted)
+        query_info_list = [(calculate_partial_score(r, 0, xi, self.Y, self.sorted_lists),
+                        xi,
+                        r,
+                        0 if xi < 0 else -1,
+                        1 if xi < 0 else -1)\
+                for r, xi in enumerate(x_u) if xi != 0]
+        query_info_list.sort(reverse=True)
+        scored = set([])
+        #  we start with the upper bound which we update iteratively
+        upper_bound = sum(map(lambda x:x[0], query_info_list))
+        lower_bound = -1e100
+        while upper_bound > lower_bound:
+            partial_score, xi, r, pos, pos_action = query_info_list[0]
+            # get item
+            item = self.sorted_lists[pos, r]
+            # score item
+            if item not in scored:
+                new_scored_item = self.score_item(x_u, item)
+                n_items_scored += 1
+                if n_items_scored < K or lower_bound < new_scored_item[0]:
+                    self.update_top_list(top_list, new_scored_item, K,
+                        n_items_scored)
+                scored.add(item)
+            # update lower bound
+            if n_items_scored >= K:
+                lower_bound = top_list[0][0]
+            # update position for this list
+            pos += pos_action
+            # update the upper bound
+            upper_bound -= partial_score  # remove previous partial score
+            partial_score = xi * self.Y[item, r]  # get new partial score
+            upper_bound += partial_score
+            # update the rule list
+            query_info_list[0] = (partial_score, xi, r, pos, pos_action)
+            cure_partial_ordered_list(query_info_list)  # keep ordering
+        if count_calculations:
+            return top_list, n_items_scored
+        else:
+            return top_list
+
 
 
 if __name__ == '__main__':
 
     import numpy as np
 
-    R = 100
+    R = 10
 
-    W = np.random.randn(10000, R)
+    W = np.random.randn(100000, R)
 
     inferer = TopKInference(W)
 
-    x = np.random.rand(R)
+    x = np.random.randn(R)
+    #x[3] = 100
 
-    top_5_list = inferer.get_top_K_naive(x, 5)
+    top_5_list_naive, n_scored_naive = inferer.get_top_K_naive(x, 5, True)
+
+    inferer.initialize_sorted_lists()
+
+    top_5_list_threshold, n_scored_threshold = inferer.get_top_K_threshold(x, 5, True)
+
+    top_5_list_threshold_enh, n_scored_threshold_enh = inferer.get_top_K_threshold_enhanced(x, 5, True)
