fb79aec13848b61679883bc3debe6554cff65dfc
MichielStock <michiel.stock@telenet.be>
2015-10-14 15:31:11 +0200

    made mode for studying regularization effect

--- Ridge_regression/regularization.py
+++ Ridge_regression/regularization.py
@@ -46,6 +46,26 @@ def regularization_settinga_kkrr(model, reg_grid, perf_measure=mse):
         performance.append(perf_measure(model._Y, Y_loocv))
     return performance
 
+def regularization_map_2srls(model, reg_grid, method='pairs', perf_measure=mse):
+    """
+    Computes the LOO performance for a two-step RLS model, given a cross
+    validation setting:
+        - pairs (default)
+        - rows
+        - columns
+        - both
+    and a given performance measure.
+    """
+    n_steps = len(reg_grid)
+    performance = np.zeros((n_steps, n_steps))
+    for i in range(n_steps):
+        reg_rows = reg_grid[i]
+        for j in range(n_steps):
+            reg_cols = reg_grid[j]
+            Y_loocv = model.predict_LOOCV_general_2SRLS(reg_rows, reg_cols,
+                    method)
+            performance[i,j] = perf_measure(model._Y, Y_loocv)
+    return performance
 
 if __name__ == '__main__':
 
@@ -58,6 +78,7 @@ if __name__ == '__main__':
     from math import exp
 
     def make_rbk_Gram(x, sigma=1.0):
+        # makes radial basis kernel matrix
         K = np.zeros((len(x), len(x)))
         for i in range(len(x)):
             for j in range(i+1):
@@ -72,11 +93,49 @@ if __name__ == '__main__':
     model = KroneckerRegularizedLeastSquares(image, K_u, K_v)
 
     reg_grid = np.logspace(-5, 5, 20)
+    reg_mesh1, reg_mesh2 = np.meshgrid(reg_grid, reg_grid)
 
     perf_setA_kkr = regularization_settinga_kkrr(model, reg_grid)
+    perf_setA_2SRLS = regularization_map_2srls(model, reg_grid, method='pairs',
+                perf_measure=mse)
+    perf_setD_2SRLS = regularization_map_2srls(model, reg_grid, method='both',
+                perf_measure=mse)
 
-    plt.plot(reg_grid, perf_setA_kkr)
-    plt.loglog()
-    plt.xlabel('lambda')
-    plt.ylabel('mean squared error')
-    plt.show()
+    # Show the results!
+    # -----------------
+
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(30, 30))
+
+    # show image
+
+    axes[0,0].imshow(image)
+
+    # for Kronecker RLS
+
+    axes[0, 1].plot(reg_grid, perf_setA_kkr)
+    axes[0, 1].loglog()
+    axes[0, 1].set_xlabel('lambda')
+    axes[0, 1].set_ylabel('mean squared error')
+
+    # for two-step RLS
+    # setting A
+    for i in range(2):
+        axes[1, i].loglog()
+        axes[1, i].set_xlabel('reg. cols')
+        axes[1, i].set_xlabel('reg. rows')
+        if i == 0:
+            axes[1, i].set_title('Holdout pairs')
+            performance = perf_setA_2SRLS
+        else:
+            axes[1, i].set_title('Holdout both')
+            performance = perf_setD_2SRLS
+        axes[1, i].contour(reg_mesh1, reg_mesh1, performance,
+                V=np.linspace(performance.min(),performance.max(),num=10), colors='k',
+                 linewidth=0.1)
+        axes[1, i].contourf(reg_mesh1, reg_mesh2, performance,
+                 cmap='hot')
+        axes[1, i].colorbar()
+
+    #cax = fig.add_axes([0.925, 0.1, 0.02, 0.8])
+    #fig.colorbar(fig, cax=cax)
+    fig.show()
