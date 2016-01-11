cc9a1dab81bceee973daaaa92a9dd0f566b1fda9
MichielStock <michiel.stock@telenet.be>
2016-01-11 20:46:55 +0100

    fist general class

--- Ridge_regression/PairwiseModel.py
+++ Ridge_regression/PairwiseModel.py
@@ -11,20 +11,20 @@ General class for pairwise model
 
 import numpy as np
 
+
 class PairwiseModel:
     """
     General pairwise model to use as a base for Kronecker ridge models and
     two-step ridge regression.
-    
+
     Has as input just the eigenvalues and eigenvectors and performs
     Kronecker ordinary least squares
     """
-    
     def __init__(self, Y, U, Sigma, V, S):
         self._Y = Y
         self._U = U
         self._V = V
-        self._sigma = Sigma
+        self._Sigma = Sigma
         self._S = S
 
     def transform_fisher(self, axis=None):
@@ -37,24 +37,80 @@ class PairwiseModel:
         the colums.
         """
         # only for binary data
-        assert self._Y.min() == 0 and self._Y.max() == 1  
+        assert self._Y.min() == 0 and self._Y.max() == 1
         self._Y = self._Y.astype(float)
-        if axis == None:
+        if axis is None:
             N = self.n_u * self.n_v * 1.0
             Npos = self._Y.sum()
             self._Y[self._Y > 0] = N / Npos
             self._Y[self._Y <= 0] = - N / (N - Npos)
-        elif axis ==  0:
+        elif axis == 0:
             rowsums = self._Y.sum(1)
             for i in range(self.n_u):
                 y = self._Y[i]
-                y[y > 0] =  self.n_v / rowsums[i]
+                y[y > 0] = self.n_v / rowsums[i]
                 y[y == 0] = - self.n_v / (self.n_v - rowsums[i])
                 self._Y[i] = y
-        elif axis ==  1:
+        elif axis == 1:
             colsums = self._Y.sum(0)
             for i in range(self.n_v):
-                y = self._Y[:,i]
-                y[y > 0] =  self.n_u / colsums[i]
-                y[y == 0] = - self.n_u / (self.n_u- colsums[i])
-                self._Y[:,i] = y
+                y = self._Y[:, i]
+                y[y > 0] = self.n_u / colsums[i]
+                y[y == 0] = - self.n_u / (self.n_u - colsums[i])
+                self._Y[:, i] = y
+
+    def _parameters_from_leverages(self, L):
+            """
+            Given the leverages, construct the parameters
+            """
+            B = L * (self._U.T).dot(self._Y).dot(self._V)
+            A = self._U.dot(B).dot(self._V.T)
+            return A
+
+    def train_model(self, return_Yhat=False):
+        """
+        Trains an Kronecker kernel ordinary least-squares model
+        """
+        # make leverages
+        L = np.dot(self._Sigma.reshape((-1, 1)), self._S.reshape((1, -1)))**-1
+        # make parameters
+        self._A = self._parameters_from_leverages(L)
+
+    def get_parameters(self):
+        return self._A
+
+    def predict(self, k=None, g=None):
+        """
+        Makes predictions
+        """
+        if k is None:
+            # use training instances
+            predictions = (self._U * self._Sigma).dot(U.T).dot(self._A)
+        else:
+            # use given instances
+            predictions = k.dot(self._A)
+        if g is None:
+            # use training tasks
+            predictions = predictions.dot(self._V * self._S).dot(self._V.T)
+        else:
+            predictions = predictions.dot(g.T)
+        return predictions
+
+if __name__ == '__main__':
+
+    Y = np.random.randn(10, 20)
+    X1 = np.random.randn(10, 10)
+    X2 = np.random.rand(20, 20)
+
+    K = X1.dot(X1.T)
+    G = X2.dot(X2.T)
+
+    Sigma, U = np.linalg.eigh(K)
+    S, V = np.linalg.eigh(G)
+
+    model = PairwiseModel(Y, U, Sigma, V, S)
+    model.train_model()
+
+    # test prediction function
+    print('These values should be the same:')
+    print(np.allclose(model.predict(), model.predict(K, G)))
